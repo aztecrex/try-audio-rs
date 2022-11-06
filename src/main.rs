d093ced65fedf566e982b68a2ac56fcdc9f1395f
Greg Wiley <aztec.rex@jammm.com>
2022-11-06 12:11:42 -0800

    refine oscillator ergonomics

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ Good starting point for integration of cpal into your application.
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn main() -> anyhow::Result<()> {
+    let (_host, device, config) = host_device_setup()?;
+
+    let synth = SineWave::new(470., config.sample_rate().0);
+
+    // let handler = make_on_data(synth, &config.into());
+
+    let stream = make_stream2(device, config, synth)?;
+
     // let stream = stream_setup_for(sample_next)?;
-    let stream = stream_setup_for(sample_next)?;
     stream.play()?;
     std::thread::sleep(std::time::Duration::from_millis(3000));
     Ok(())
@@ -90,23 +97,68 @@ impl Interval {
     }
 }
 
-fn sample_next(osc: &mut impl Oscillator) -> f32 {
-    osc.next()
+fn make_stream2(
+    device: cpal::Device,
+    config: cpal::SupportedStreamConfig,
+    synth: impl Oscillator + Send + 'static,
+) -> Result<cpal::Stream, anyhow::Error> {
+    let err_fn = |err| eprintln!("Error in stream: {}", err);
+    let channels = config.channels();
+    let format = config.sample_format();
+    let config: cpal::StreamConfig = config.into();
+    let stream = match format {
+        cpal::SampleFormat::F32 => {
+            device.build_output_stream(&config, make_on_data::<f32>(synth, channels), err_fn)
+        }
+        cpal::SampleFormat::I16 => {
+            device.build_output_stream(&config.into(), make_on_data::<i16>(synth, channels), err_fn)
+        }
+        cpal::SampleFormat::U16 => {
+            device.build_output_stream(&config.into(), make_on_data::<u16>(synth, channels), err_fn)
+        }
+    }?;
+
+    Ok(stream)
 }
 
-pub fn stream_setup_for<F, O>(on_sample: F) -> Result<cpal::Stream, anyhow::Error>
-where
-    O: Oscillator,
-    F: FnMut(&mut O) -> f32 + Send + 'static + Copy,
-{
-    let (_host, device, config) = host_device_setup()?;
+// fn make_stream<O>(
+//     device: cpal::Device,
+//     config: cpal::SupportedStreamConfig,
+//     synth: O,
+// ) -> Result<cpal::Stream, anyhow::Error>
+// where
+//     O: Oscillator + Send,
+// {
+//     let handler = match config.sample_format() {
+//         cpal::SampleFormat::F32 => make_on_data::<f32>(synth, &config.into()),
+//         cpal::SampleFormat::I16 => make_on_data::<i16>(synth, &config.into()),
+//         cpal::SampleFormat::U16 => make_on_data::<u16>(synth, &config.into()),
+//     };
 
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample),
-    }
-}
+//     match config.sample_format() {
+//         cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample),
+//         cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample),
+//         cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample),
+//     }
+// }
+
+// fn sample_next(osc: &mut impl Oscillator) -> f32 {
+//     osc.next()
+// }
+
+// pub fn stream_setup_for<F, O>(on_sample: F) -> Result<cpal::Stream, anyhow::Error>
+// where
+//     O: Oscillator,
+//     F: FnMut(&mut O) -> f32 + Send + 'static + Copy,
+// {
+//     let (_host, device, config) = host_device_setup()?;
+
+//     match config.sample_format() {
+//         cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample),
+//         cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample),
+//         cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample),
+//     }
+// }
 
 pub fn host_device_setup(
 ) -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
@@ -123,42 +175,62 @@ pub fn host_device_setup(
     Ok((host, device, config))
 }
 
-pub fn stream_make<T, F, O>(
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-    on_sample: F,
-) -> Result<cpal::Stream, anyhow::Error>
+// pub fn stream_make<Sam, F, O>(
+//     device: &cpal::Device,
+//     config: &cpal::StreamConfig,
+//     on_sample: F,
+// ) -> Result<cpal::Stream, anyhow::Error>
+// where
+//     O: Oscillator,
+//     Sam: cpal::Sample,
+//     F: FnMut(&mut O) -> f32 + std::marker::Send + 'static + Copy,
+// {
+//     let sample_rate = config.sample_rate.0;
+//     // let sample_clock = 0f32;
+//     let nchannels = config.channels as usize;
+//     let mut osc = SineWave::new(440., sample_rate);
+//     let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+
+//     let stream = device.build_output_stream(
+//         config,
+//         move |output: &mut [Sam], _: &cpal::OutputCallbackInfo| {
+//             on_window(output, nchannels, &mut osc, on_sample)
+//         },
+//         err_fn,
+//     )?;
+
+//     Ok(stream)
+// }
+
+// pub struct Stream(cpal::Stream);
+
+fn make_on_data<Sam>(
+    mut osc: impl Oscillator + Send,
+    num_channels: u16,
+) -> impl FnMut(&mut [Sam], &cpal::OutputCallbackInfo) + Send
 where
-    O: Oscillator,
-    T: cpal::Sample,
-    F: FnMut(&mut O) -> f32 + std::marker::Send + 'static + Copy,
+    Sam: cpal::Sample,
 {
-    let sample_rate = config.sample_rate.0;
-    // let sample_clock = 0f32;
-    let nchannels = config.channels as usize;
-    let mut osc = SineWave::new(440., sample_rate as u32);
-    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
-
-    let stream = device.build_output_stream(
-        config,
-        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
-            on_window(output, nchannels, &mut osc, on_sample)
-        },
-        err_fn,
-    )?;
-
-    Ok(stream)
-}
-
-fn on_window<T, F, O>(output: &mut [T], nchannels: usize, osc: &mut O, mut on_sample: F)
-where
-    T: cpal::Sample,
-    F: FnMut(&mut O) -> f32 + std::marker::Send + 'static,
-{
-    for frame in output.chunks_mut(nchannels) {
-        let value: T = cpal::Sample::from::<f32>(&on_sample(osc));
-        for sample in frame.iter_mut() {
-            *sample = value;
+    let num_channels = num_channels as usize;
+    move |output: &mut [Sam], _: &cpal::OutputCallbackInfo| {
+        for frame in output.chunks_mut(num_channels) {
+            let osc_sample = cpal::Sample::from::<f32>(&osc.next());
+            for out in frame.iter_mut() {
+                *out = osc_sample;
+            }
         }
     }
 }
+
+// fn on_window<T, F, O>(output: &mut [T], nchannels: u16, osc: &mut O, mut on_sample: F)
+// where
+//     T: cpal::Sample,
+//     F: FnMut(&mut O) -> f32 + std::marker::Send + 'static,
+// {
+//     for frame in output.chunks_mut(nchannels) {
+//         let value: T = cpal::Sample::from::<f32>(&on_sample(osc));
+//         for sample in frame.iter_mut() {
+//             *sample = value;
+//         }
+//     }
+// }
